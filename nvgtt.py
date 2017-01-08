2d6565964e430dc412abe76f756beafa072ba3f9
Lucas <lucas.o@live.com>
2017-01-08 21:17:11 -0200

    progress

--- nvgtt.py
+++ nvgtt.py
@@ -1,110 +1,68 @@
-from bs4 import BeautifulSoup
-
-#import wikipedia
-
-#from nltk import word_tokenize
-
 import wikipydia
 
 import re
 
-import sys
 
-#Module to render graphics
-#import matplotlib.pyplot as plt
+def getLinksScore(pageName):
+    #Download page structured by sections
+    pageData = wikipydia.getPage(pageName)
+    soup = pageData['full']
 
-#User default dict because it generates a default value according to the datatype not found
-from collections import defaultdict
+    #Clear table of contents if any
+    for node in soup.findAll(id='toc'):
+        node.decompose()
 
-from nltk.tokenize import word_tokenize
+    #Clear top info table if any
+    for node in soup.findAll(class_='ambox'):
+        node.decompose()
 
-import nltk.draw.dispersion as disp
+    #Clear info box if any
+    for node in soup.findAll(class_='infobox'):
+        node.decompose()
 
-
-def saveAndExit(htmlData):
-    """Debug function to save html data to a file and exit script."""
-
-    f = open('teste2.html', 'w')
-    f.write(htmlData.encode('utf-8'))
-    f.close()
-    sys.exit(0)
-
-
-def clearSectionAndBelow(html, sectionId):
-    """Function to remove the target section and content below it in a wikipedia article."""
-
-    clearNode = html.find(id=sectionId)
-
-    if clearNode:
-
-        #Create list of nodes to be removed
-        #Check if they are valid nodes by the name property
-        decomposeQueue = [siblingNode for siblingNode in clearNode.parent.next_siblings if siblingNode.name != None]
-        decomposeQueue.append(clearNode.parent)
-
-        #Remove all nodes on the list
-        for nodeToDelete in decomposeQueue:
-            nodeToDelete.decompose()
+    #Clear verticalbox if any
+    for node in soup.findAll(class_='vertical-navbox'):
+        node.decompose()
 
 
+    #Get the wikilinks texts and build a set of target word
+    #Avoid links with : that means special links for wikipedia
+    #Clear parentheses in text cause it is not handled proper by text boundaries \b
+    #targetWords = set([re.sub(r" \(.*\)", "", link.text.lower()) for link in soup.findAll('a') if link['href'][0:6] == '/wiki/' and not ':' in link['href']])
+    targetLinks = set([(link['href'][6:], link.text.lower()) for link in soup.findAll('a') if link['href'][0:6] == '/wiki/' and not ':' in link['href']])
+    #check why the "for" word is appearing on python
+    #test without removing parentheses
+
+    sectionsSoup = pageData['sections']
+
+    #Gen section texts based on article sections
+    sectionTexts = []
+    for section in sectionsSoup:
+        sectionTexts.append(''.join([tag.text for tag in section]))
+
+    presVects = []
+
+    #Improve later with better importance algorithm and using vectors of meanings based on other articles links text
+    importVector = getImportanceVector(sectionTexts)
+
+    #MAYBE COUNT THE NUMBER OF TIMES A WORD APPEAR IN THE TEXT FOR ADITIONAL WEIGHT
+    #NOW MUST THINK ABOUT THE INTERFACE AND WHERE WILL BE THE START POINT TO MEASURE THE HWO FAR
+
+    for vec in getPresenceVector(sectionTexts, targetLinks).items():
+        vecWeight = getVectorWeight(vec[1], importVector)
+
+        presVects.append((vec[0], vec[1], vecWeight))
 
 
-#---- main() ----#
+    return presVects
 
 
-pageData = wikipydia.getPage(sys.argv[1])
-soup = pageData['full']
-
-# for p in soup.findAll('p'):
-#     print p.text.encode('utf-8')
-#     print '\n'
-
-
-# sys.exit(0)
-
-
-#USER 1+ LOG OF FREQUENCY AS MEASUREMENT
-
-#Use something like the kalman filters to estimate the orders of important things
-
-### Treat Data ###
-
-#Clear table of contents if any
-for node in soup.findAll(id='toc'):
-    node.decompose()
-
-#Clear top info table if any
-for node in soup.findAll(class_='ambox'):
-    node.decompose()
-
-#Clear info box if any
-for node in soup.findAll(class_='infobox'):
-    node.decompose()
-
-#Clear verticalbox if any
-for node in soup.findAll(class_='vertical-navbox'):
-    node.decompose()
-
-
-#Clear everything below some of the sections below
-# clearSections = ['References', 'See_also']
-
-# for section in clearSections:
-#     clearSectionAndBelow(soup, section)
-
-
-# for i, section in enumerate(pageData['sections']):
-#     print "SECTION " + str(i) + "\n"
-#     print '\n'.join([tag.text for tag in section]).encode('utf-8')
-#     print "\n\n"
-
-#print re.findall('\\bMCU \(Micro Controller Unit\)', soup.text, re.IGNORECASE)
-
-def getPresenceVector(texts, targetWords):
+def getPresenceVector(texts, targetLinks):
     wordDict = dict()
 
     #Iterate thru all target words
-    for word in targetWords:
+    for link in targetLinks:
+        word = link[1] #Get link text
         wordDict[word] = []
         #Iterate thru all texts 
         for text in texts:
@@ -134,49 +92,23 @@ def getVectorWeight(targetVector, importanceVector):
     
     return valSum
 
-#Get the wikilinks texts and build a set of target word
-#Avoid links with : that means special links for wikipedia
-#Clear parentheses in text cause it is not handled proper by text boundaries \b
-#targetWords = set([re.sub(r" \(.*\)", "", link.text.lower()) for link in soup.findAll('a') if link['href'][0:6] == '/wiki/' and not ':' in link['href']])
-targetWords = set([link.text.lower() for link in soup.findAll('a') if link['href'][0:6] == '/wiki/' and not ':' in link['href']])
-#check why the "for" word is appearing on python
-#test without removing parentheses
 
-sectionsSoup = pageData['sections']
 
-# for section in sectionsSoup:
-#     print ''.join([tag.text for tag in section]).encode('utf-8')
-#     print "\n\n"
 
-# sys.exit(0)
 
-# for word in targetWords:
-#     print word
-#     print "\n"
+#Test stuff
+if __name__ == "__main__":
+    import sys
 
-#Gen section texts based on article sections
-sectionTexts = []
-for section in sectionsSoup:
-    sectionTexts.append(''.join([tag.text for tag in section]))
+    def getVectVal(vect):
+        return vect[2]
 
-presVects = []
+    presVects = getLinksScore(sys.argv[1])
 
-#Improve later with better importance algorithm and using vectors of meanings based on other articles links text
-importVector = getImportanceVector(sectionTexts)
+    presVects.sort(key=getVectVal, reverse=True)
 
-#MAYBE COUNT THE NUMBER OF TIMES A WORD APPEAR IN THE TEXT FOR ADITIONAL WEIGHT
-#NOW MUST THINK ABOUT THE INTERFACE AND WHERE WILL BE THE START POINT TO MEASURE THE HWO FAR
+    for vect in presVects:
+        print vect[0].encode('utf-8'), vect[1], vect[2]
 
-for vec in getPresenceVector(sectionTexts, targetWords).items():
-    vecWeight = getVectorWeight(vec[1], importVector)
 
-    presVects.append((vec[0], vec[1], vecWeight))
-    
 
-def getVectVal(vect):
-    return vect[2]
-
-presVects.sort(key=getVectVal, reverse=True)
-
-for vect in presVects:
-    print vect[0].encode('utf-8'), vect[1], vect[2]
---
+++ server.py
@@ -0,0 +1,66 @@
+import os
+import cherrypy
+import numpy as np
+
+from nvgtt import getLinksScore
+
+
+class IndexHandler(object):
+    @cherrypy.expose
+    def index(self):
+        # return open('public/index.html')
+        return 'lucas'
+
+class UploadHandler(object):
+    exposed = True
+
+    def GET(self, article):
+
+        def getVectVal(vect):
+            return vect[2]
+
+        presVects = getLinksScore(article)
+
+        presVects.sort(key=getVectVal, reverse=True)
+
+        resultArr = []
+
+        for vect in presVects:
+            resultArr.append(vect[0].encode('utf-8') + " " + str(vect[2]))
+
+        return '<html><body><span style="font-size:16px;">' + '<br>'.join(resultArr) + '</span></body></html>'
+
+must think how to keep going
+    def POST(self, file):
+
+        return 'post'
+     
+
+
+if __name__ == '__main__':
+
+    conf = {
+        '/': {
+            'tools.staticdir.on': True,
+            'tools.staticdir.root': os.path.abspath(os.getcwd()),
+            'tools.staticdir.dir': './public'
+        },
+
+        '/upload':{
+            'request.dispatch': cherrypy.dispatch.MethodDispatcher(),
+            'tools.response_headers.on': True,
+            # 'tools.response_headers.headers': [('Content-Type', 'text/plain')]
+        }
+    }
+
+    webapp = IndexHandler()
+    webapp.upload = UploadHandler()
+
+    # Read port selected by the cloud for our application
+    #PORT = int(os.getenv('PORT', 8080))
+    PORT = 80
+
+    # Set server port
+    cherrypy.config.update({'server.socket_host': '0.0.0.0', 'server.socket_port': PORT})
+
+    cherrypy.quickstart(webapp, '/', conf)
